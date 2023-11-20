119657fc63cc88359845fdbb301c05e8e514066f
Alexandre <alexandremartinx@gmail.com>
2023-11-20 07:02:25 -0300

    alteração

--- sheets_products.js
+++ sheets_products.js
@@ -5,7 +5,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("O produto foi cadastrado com sucesso!" ))
+  .then(response => alert("Seu presente foi indicado com sucesso!" ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
