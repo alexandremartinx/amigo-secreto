ac61c9246d7eccc873292814a597a58c3e271745
Alexandre <alexandremartinx@gmail.com>
2023-11-20 07:39:02 -0300

    remoção de dropdown

--- sheets_products.js
+++ sheets_products.js
@@ -1,13 +1,3 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-  var dropdown = document.getElementById("pessoa");
-
-  dropdown.addEventListener("change", function() {
-      var selectedColor = dropdown.value;
-      alert("Bem-vindo " + selectedColor);
-  });
-});
-
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz9ByN2sUXDJBw5yaHRCJh7GYQMRqhT5z9ga-ftUd8W5HL5eeoFuiDeNARGqDLcv-dD/exec'
 
 const form = document.forms['contact-form']
@@ -15,16 +5,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Seu presente foi indicado com sucesso!" ))
+  .then(response => alert("Sua sugestão foi indicado com sucesso!" ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
-
-document.addEventListener("DOMContentLoaded", function() {
-  var dropdown = document.getElementById("pessoa");
-
-  dropdown.addEventListener("change", function() {
-      var selectedColor = dropdown.value;
-      alert("Bem-vindo " + selectedColor);
-  });
-});
